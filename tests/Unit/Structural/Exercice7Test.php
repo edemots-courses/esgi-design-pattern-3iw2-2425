28ad7effa45f85885c2aa232c22c04b30d8e7e82
Eliot Demots <eliot@demots.fr>
2025-04-16 18:37:45 +0200

    update tests

--- tests/Unit/Structural/Exercice7Test.php
+++ tests/Unit/Structural/Exercice7Test.php
@@ -1,10 +1,13 @@
 <?php
 
+use EdemotsCourses\EsgiDesignPattern\Exercice7\Entity;
 use EdemotsCourses\EsgiDesignPattern\Exercice7\FormatterInterface;
+use EdemotsCourses\EsgiDesignPattern\Exercice7\JsonFormatter;
 use EdemotsCourses\EsgiDesignPattern\Exercice7\Product;
 use EdemotsCourses\EsgiDesignPattern\Exercice7\ProductDataRenderer;
 use EdemotsCourses\EsgiDesignPattern\Exercice7\User;
 use EdemotsCourses\EsgiDesignPattern\Exercice7\UserDataRenderer;
+use EdemotsCourses\EsgiDesignPattern\Exercice7\XmlFormatter;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Test;
 use PHPUnit\Framework\TestCase;
@@ -13,21 +16,43 @@ class Exercice7Test extends TestCase
 {
     #[Test]
     #[DataProvider('userDataProvider')]
-    public function itRenderUserWithFormatter(array $data)
+    public function itRenderUserWithFormatter(...$data)
     {
         $formatter = $this->createMock(FormatterInterface::class);
         $formatter->expects($this->once())
             ->method('format')
             ->with($data);
 
-        $userMock = $this->getMockBuilder(User::class)
-            ->disableOriginalConstructor()
-            ->getMock()
-            ->method('toArray')
+        $entityMock = $this->createMock(Entity::class);
+        $entityMock->method('toArray')
             ->willReturn($data);
 
         $userRenderer = new UserDataRenderer($formatter);
-        $userRenderer->render($userMock);
+        $userRenderer->render($entityMock);
+    }
+
+    #[Test]
+    #[DataProvider('userDataProvider')]
+    public function itFormatUserInJson(...$data)
+    {
+        $userRenderer = new UserDataRenderer(new JsonFormatter());
+        $result = $userRenderer->render(new User(...$data));
+
+        $this->assertEquals(json_encode($data, JSON_PRETTY_PRINT), $result);
+    }
+
+    #[Test]
+    #[DataProvider('userDataProvider')]
+    public function itFormatUserInXml(...$data)
+    {
+        $userRenderer = new UserDataRenderer(new XmlFormatter());
+        $result = $userRenderer->render(new User(...$data));
+
+        $this->assertEquals(<<<EOL
+<?xml version="1.0"?>
+<root><id>1</id><name>Toto</name><email>toto@example.com</email></root>
+
+EOL, $result);
     }
 
     public static function userDataProvider()
@@ -39,21 +64,44 @@ class Exercice7Test extends TestCase
 
     #[Test]
     #[DataProvider('productDataProvider')]
-    public function itRenderProductWithFormatter(array $data)
+    public function itRenderProductWithFormatter(...$data)
     {
         $formatter = $this->createMock(FormatterInterface::class);
         $formatter->expects($this->once())
             ->method('format')
             ->with($data);
 
-        $productMock = $this->getMockBuilder(Product::class)
-            ->disableOriginalConstructor()
-            ->getMock()
-            ->method('toArray')
+        $productMock = $this->createMock(Entity::class);
+        $productMock->method('toArray')
             ->willReturn($data);
 
         $userRenderer = new ProductDataRenderer($formatter);
         $userRenderer->render($productMock);
+    }
+
+
+    #[Test]
+    #[DataProvider('productDataProvider')]
+    public function itFormatProductInJson(...$data)
+    {
+        $productRenderer = new ProductDataRenderer(new JsonFormatter());
+        $result = $productRenderer->render(new Product(...$data));
+
+        $this->assertEquals(json_encode($data, JSON_PRETTY_PRINT), $result);
+    }
+
+    #[Test]
+    #[DataProvider('productDataProvider')]
+    public function itFormatProductInXml(...$data)
+    {
+        $productRenderer = new ProductDataRenderer(new XmlFormatter());
+        $result = $productRenderer->render(new Product(...$data));
+
+        $this->assertEquals(<<<EOL
+<?xml version="1.0"?>
+<root><id>1</id><name>Super produit</name><price>3.33</price></root>
+
+EOL, $result);
     }
 
     public static function productDataProvider()
